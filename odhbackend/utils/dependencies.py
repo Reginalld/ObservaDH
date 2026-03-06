c892980312d7b6fd3d7319eb08d1e52a55c6ff18
Reginald <reginaldosgomesj@gmail.com>
2026-03-06 12:56:40 -0300

    Mudanças de inicialização e variaveis de ambiente relacionadas aos bancos de dados.

--- odhbackend/utils/dependencies.py
+++ odhbackend/utils/dependencies.py
@@ -1,11 +1,17 @@
 import os
 from elasticsearch import Elasticsearch
+from fastapi import Depends
+from sqlmodel import Session
+
+from odhbackend.services.structured_data_store_service import get_session
+
 from odhbackend.repositories.es_article_repository import ESArticleRepository
 from odhbackend.services.article_service import ArticleService
 
 _host = 'elasticsearch_container' if os.path.exists('/.dockerenv') else 'localhost'
 es_client = Elasticsearch([f"http://{_host}:9200"])
 
-def get_article_service() -> ArticleService:
+def get_article_service(db: Session = Depends(get_session)) -> ArticleService:
     repo = ESArticleRepository(client=es_client)
-    return ArticleService(repository=repo)
+    
+    return ArticleService(repository=repo, db=db)
